39bf9aea19cf37f9dedf65b6762a8d278170e747
caaatisgood <caaatisgood@gmail.com>
2023-08-06 17:02:41 +0100

    update homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,8 +9,12 @@ export default function Home() {
         <title>punches | make consistent progress toward your goal.</title>
       </Head>
       <h1>
-        <code className="text-xs">punches / make consistent progress toward your goal / coming soon.</code>
+        <code className="text-xs">punches / make consistent progress toward your goal / coming soon</code>
       </h1>
+      <br />
+      <p>
+        <code className="text-xs">follow <a className="underline" href="https://twitter.com/caaatisgood" target="_blank">@caaatisgood</a> for updates</code>
+      </p>
     </main>
   )
 }
