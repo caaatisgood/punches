d8bdfc717239723902c85896fea20c367c7eeebb
caaatisgood <caaatisgood@gmail.com>
2023-08-06 15:08:06 +0100

    init

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-center`}
+    >
+      <Head>
+        <title>punches | make consistent progress toward your goal.</title>
+      </Head>
+      <h1>
+        <code className="text-xs">punches / make consistent progress toward your goal / coming soon.</code>
+      </h1>
+    </main>
+  )
+}
