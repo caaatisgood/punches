40fcbe59956489a31cb2aed62895f744b4260869
caaatisgood <caaatisgood@gmail.com>
2023-08-21 22:42:42 +0200

    minor ui update

---
+++ frontend/config/index.ts
@@ -0,0 +1,1 @@
+export const KT_ADDRESS = "KT1VHEFbTqNPDTEXx4TDeXPnyErk4CGS7LRU";
--- frontend/pages/next.tsx
+++ frontend/pages/next.tsx
@@ -4,8 +4,21 @@ import { getWallet } from '@/utils/wallet'
 
 const WIP_MAX_CHARS = 100
 
+const PLACEHOLDERS = [
+  "building an app to help ppl make consistent progress toward their goal",
+]
+
+const local = {
+  getWip: () =>
+    String(typeof window !== "undefined" && window?.localStorage.getItem("punches_wip") || ""),
+  setWip: (wip: string) =>
+    typeof window !== "undefined" && window?.localStorage.setItem("punches_wip", wip),
+}
+
+const randPlaceholder = PLACEHOLDERS[Math.floor((Math.random() * PLACEHOLDERS.length))]
+
 const Page = () => {
-  const [wip, setWip] = useState("")
+  const [wip, setWip] = useState(local.getWip() || "")
   // const divRef = useRef<HTMLDivElement>(null)
 
   // useEffect(() => {
@@ -26,6 +39,11 @@ const Page = () => {
   //   }
   // }, [divRef])
 
+  const updateWip = (wip: string) => {
+    setWip(wip)
+    local.setWip(wip)
+  }
+
   const onSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault()
     const wallet = getWallet()
@@ -44,7 +62,13 @@ const Page = () => {
     <main
       className={`font-sans flex min-h-screen flex-col items-center justify-center p-4`}
     >
-      <p className="mb-4 text-center">sup, glad to see you here. what are you working on?</p>
+      {/* <h1 className="mb-4 text-center">
+        punches / make consistent progress toward your goal
+      </h1>
+      <br />
+      <br />
+      <br /> */}
+      <p className="mb-4 text-center">sup, glad to see you here. what is your work-in-progress?</p>
       {/* <div
         ref={divRef}
         className="mb-4 text-center text-xl w-full sm:w-2/3 lg:w-2/5 xl:w-96 min-h-1 py-3 outline-none"
@@ -61,21 +85,24 @@ const Page = () => {
       <form className="flex flex-col w-full items-center" onSubmit={onSubmit}>
         <input
           name="wip"
-          className="mb-4 bg-neutral-900 text-center text-xl w-full sm:w-2/3 lg:w-3/5 xl:w-[50rem] min-h-1 py-3 px-2 outline-none rounded-md"
+          className="mb-4 bg-neutral-900 text-center text-l w-full sm:w-2/3 lg:w-3/5 xl:w-[50rem] min-h-1 py-3 px-2 outline-none rounded-md"
           value={wip}
           maxLength={WIP_MAX_CHARS}
           type='text'
           autoFocus
           required
           onChange={(evt) => {
-            setWip(evt.target.value)
+            updateWip(evt.target.value)
           }}
+          placeholder={randPlaceholder}
         />
         <p className="text-xs">
           {wip.length}/{WIP_MAX_CHARS}
         </p>
+        <br />
         <button
-          className='px-2 py-1'
+          className="shadow-lg shadow-neutral-500/50 px-2 py-1 rounded-md bg-neutral-100 text-neutral-900 disabled:cursor-not-allowed disabled:opacity-80 disabled:shadow-none"
+          disabled={wip.length === 0}
           type="submit"
         >
           punch
