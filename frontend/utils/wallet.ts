fbd9e6369cd039ced8f8462349c332c534b8b5c6
caaatisgood <caaatisgood@gmail.com>
2023-08-29 23:09:25 +0200

    clean up legacy stuff

--- frontend/utils/wallet.ts
+++ frontend/utils/wallet.ts
@@ -1,35 +1,7 @@
-import { BeaconWallet } from '@taquito/beacon-wallet';
-import { NetworkType } from '@airgap/beacon-types'
-
 import { Tezos } from '@/store/userStore'
 import { House } from '@/types'
 
 import { KT_ADDRESS } from '@/config'
-
-let wallet: BeaconWallet
-
-export const getWallet = (): BeaconWallet => {
-  if (wallet) {
-    return wallet
-  }
-
-  wallet = new BeaconWallet({
-    name: 'Punches',
-    // iconUrl: 'https://tezostaquito.io/img/favicon.svg',
-    preferredNetwork: NetworkType.GHOSTNET,
-    eventHandlers: {
-      PERMISSION_REQUEST_SUCCESS: {
-        handler: async (data: any) => {
-          console.log('permission data:', data);
-        },
-      },
-    },
-  });
-  
-  Tezos.setWalletProvider(wallet);
-
-  return wallet
-}
 
 type KtCallOptions = {
   onWaitingToBeConfirmed?: (hash: string) => void;
