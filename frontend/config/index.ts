1b6c3b9fb87fd1b104abfbf2c27c0f459d9c66b1
caaatisgood <caaatisgood@gmail.com>
2023-08-31 10:27:10 +0200

    kt config fix

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,3 +1,3 @@
 export const KT_ADDRESS = process.env.NEXT_PUBLIC_MAINNET
-  ? process.env.NEXT_PUBLIC_MAINNET_KT_ADDRESS
-  : process.env.NEXT_PUBLIC_GHOST_KT_ADDRESS;
+  ? process.env.NEXT_PUBLIC_MAINNET_KT_ADDRESS!
+  : process.env.NEXT_PUBLIC_GHOST_KT_ADDRESS!;
